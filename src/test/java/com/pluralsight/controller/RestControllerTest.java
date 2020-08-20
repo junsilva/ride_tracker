ab808e362480e920be20d0082b545306c90b3b2f
junsilva <learn.silva.angelito@gmail.com>
2020-08-20 23:21:27 +1000

    Error Handling
    created class ServiceError for error handling
    registered new ExceptionHandler and catch all RuntimeExceptions.  Class ServiceError used to return response
    created test methods to trigger

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -79,4 +79,12 @@ public class RestControllerTest {
 		
 	}
 	
+	@Test(timeout=10000)
+	public void testException() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.getForObject("http://localhost:8080/ride_tracker/test", Ride.class);
+		
+	}
+	
 }
